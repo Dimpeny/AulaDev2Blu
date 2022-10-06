511fadf30f5efb195559514ee4583cbe05ebd5b4
Dimpeny <guilhermezimermann.grz@gmail.com>
2022-10-05 21:04:17 -0300

    Formulario de cadastro adicionado

--- SolutionAulaBD4/SlnSistemaCadastro/src/Devs2Blu.ProjetosAula.SistemaCadastro.Forms/Form1.Designer.cs
+++ SolutionAulaBD4/SlnSistemaCadastro/src/Devs2Blu.ProjetosAula.SistemaCadastro.Forms/Form1.Designer.cs
@@ -28,21 +28,142 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.cboConvenio = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rbFisica = new System.Windows.Forms.RadioButton();
+            this.rbJuridica = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblCGCCPF = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // cboConvenio
+            // 
+            this.cboConvenio.FormattingEnabled = true;
+            this.cboConvenio.Location = new System.Drawing.Point(74, 13);
+            this.cboConvenio.Name = "cboConvenio";
+            this.cboConvenio.Size = new System.Drawing.Size(121, 21);
+            this.cboConvenio.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Convênio:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblCGCCPF);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cboConvenio);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(258, 416);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Formulario de Cadastro";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rbJuridica);
+            this.groupBox2.Controls.Add(this.rbFisica);
+            this.groupBox2.Location = new System.Drawing.Point(16, 59);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 56);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Tipo de Pessoa";
+            // 
+            // rbFisica
+            // 
+            this.rbFisica.AutoSize = true;
+            this.rbFisica.Location = new System.Drawing.Point(7, 20);
+            this.rbFisica.Name = "rbFisica";
+            this.rbFisica.Size = new System.Drawing.Size(52, 17);
+            this.rbFisica.TabIndex = 0;
+            this.rbFisica.TabStop = true;
+            this.rbFisica.Text = "Fisica";
+            this.rbFisica.UseVisualStyleBackColor = true;
+            this.rbFisica.CheckedChanged += new System.EventHandler(this.rbFisica_CheckedChanged);
+            // 
+            // rbJuridica
+            // 
+            this.rbJuridica.AutoSize = true;
+            this.rbJuridica.Location = new System.Drawing.Point(118, 20);
+            this.rbJuridica.Name = "rbJuridica";
+            this.rbJuridica.Size = new System.Drawing.Size(61, 17);
+            this.rbJuridica.TabIndex = 1;
+            this.rbJuridica.TabStop = true;
+            this.rbJuridica.Text = "Juridica";
+            this.rbJuridica.UseVisualStyleBackColor = true;
+            this.rbJuridica.CheckedChanged += new System.EventHandler(this.rbJuridica_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nome:";
+            // 
+            // lblCGCCPF
+            // 
+            this.lblCGCCPF.AutoSize = true;
+            this.lblCGCCPF.Location = new System.Drawing.Point(19, 139);
+            this.lblCGCCPF.Name = "lblCGCCPF";
+            this.lblCGCCPF.Size = new System.Drawing.Size(49, 13);
+            this.lblCGCCPF.TabIndex = 3;
+            this.lblCGCCPF.Text = "CGCCPF";
+            this.lblCGCCPF.Visible = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Location = new System.Drawing.Point(290, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(276, 416);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "groupBox3";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cboConvenio;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblCGCCPF;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rbJuridica;
+        private System.Windows.Forms.RadioButton rbFisica;
+        private System.Windows.Forms.GroupBox groupBox3;
     }
 }
 
--- SolutionAulaBD4/SlnSistemaCadastro/src/Devs2Blu.ProjetosAula.SistemaCadastro.Forms/Form1.cs
+++ SolutionAulaBD4/SlnSistemaCadastro/src/Devs2Blu.ProjetosAula.SistemaCadastro.Forms/Form1.cs
@@ -15,6 +15,8 @@ namespace Devs2Blu.ProjetosAula.SistemaCadastro.Forms
     public partial class Form1 : Form
     {
         public MySqlConnection Conn { get; set; }
+        public ConvenioRepository ConvenioRepository { get; set; }
+
         public Form1()
         {
             InitializeComponent();
@@ -22,13 +24,47 @@ namespace Devs2Blu.ProjetosAula.SistemaCadastro.Forms
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            ConvenioRepository = new ConvenioRepository();
             Conn = ConnectionMySQL.GetConnection();
 
             if (Conn.State == ConnectionState.Open)
             {
 
                 MessageBox.Show("Conexão efetuada com sucesso!", "Conexão MySQL!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                
                 Conn.Close();
+
+            }
+            PopulaComboBox();
+        }
+
+        #region Metodos
+        public  void PopulaComboBox()
+        {
+            var listConvenios = ConvenioRepository.FetchAll();
+
+            while (listConvenios.Read())
+            {
+                cboConvenio.Items.Add(listConvenios.GetString("nome"));
+            }
+        }
+        #endregion
+
+        private void rbFisica_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbFisica.Checked)
+            {
+                lblCGCCPF.Text = "CPF:";
+                lblCGCCPF.Visible = true;
+            }
+        }
+
+        private void rbJuridica_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbJuridica.Checked)
+            {
+                lblCGCCPF.Text = "CNPJ:";
+                lblCGCCPF.Visible = true;
             }
         }
     }
