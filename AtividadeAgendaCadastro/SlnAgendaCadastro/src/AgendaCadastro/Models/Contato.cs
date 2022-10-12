efe40796c4f05c65627980eba433e2689afc9a71
Dimpeny <guilhermezimermann.grz@gmail.com>
2022-10-11 21:25:30 -0300

    Criado Enum Status, e Models Contato e Estado

---
+++ AtividadeAgendaCadastro/SlnAgendaCadastro/src/AgendaCadastro/Models/Contato.cs
@@ -0,0 +1,23 @@
+﻿using AgendaCadastro.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaCadastro.Models
+{
+    public class Contato
+    {
+        public Int32 Id { get; set; }
+        public String Nome { get; set; }
+        public String NumeroTelefonico { get; set; }
+        public String Cep { get; set; }
+        public Int32 Numero { get; set; }
+        public String Rua { get; set; }
+        public String Bairro { get; set; }
+        public String Cidade { get; set; }
+        public Estado Estado { get; set; }
+        public Status Status { get; set; }
+    }
+}
---
+++ AtividadeAgendaCadastro/SlnAgendaCadastro/src/AgendaCadastro/Models/Estado.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaCadastro.Models
+{
+    public class Estado
+    {
+        public Int32 Id { get; set; }
+        public string Uf { get; set; }
+        public string Descricao { get; set; }
+
+        public Estado()
+        {
+
+        }
+
+        public Estado(int id, string uf, string descricao)
+        {
+            Id = id;
+            Uf = uf;
+            Descricao = descricao;
+        }
+    }
+}
