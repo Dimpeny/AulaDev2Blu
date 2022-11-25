301c88816d663715371beacb47faaa14c3206518
Dimpeny <guilhermezimermann.grz@gmail.com>
2022-11-25 19:01:59 -0300

    Adicionada pasta CSharpMVC

---
+++ CSharpMVC/SlnPrimeiroProjetoMVC/src/Testemvc/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Testemvc.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Indicadores()
+        {
+            return View();
+        }
+    }
+}
---
+++ CSharpMVC/SlnPrimeiroProjetoMVC/src/Testemvc/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Testemvc.Models;
+
+namespace Testemvc.Controllers
+{
+    //[Route("")]
+    //[Route("home")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        [Route("home/Cadastro")]
+        [Route("Cadastre-se")]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
