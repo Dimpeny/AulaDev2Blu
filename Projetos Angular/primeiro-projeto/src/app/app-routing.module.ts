522f714f116c6404e088f41e87d5055e2a13db89
Dimpeny <guilhermezimermann.grz@gmail.com>
2023-02-16 22:10:46 -0300

    Termino da aula 16/02

---
+++ Projetos Angular/primeiro-projeto/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { FormComponent } from './components/form/form.component';
+import { HomeComponent } from './components/home/home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+const routes: Routes = [
+  {path:"home",component: HomeComponent },
+  {path: "form", component : FormComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
