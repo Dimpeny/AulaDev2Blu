acfa5dc1e8b833337a130c895955f2c985bc4ea0
Dimpeny <guilhermezimermann.grz@gmail.com>
2023-02-17 21:19:38 -0300

    Criado projeto-consumo-api

---
+++ Projetos Angular/projeto-consumo-api/src/app/service/api.service.ts
@@ -0,0 +1,11 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  URL_API:string = "https://pokeapi.co/api/v2"
+  constructor(public http:HttpClient) {}
+}
