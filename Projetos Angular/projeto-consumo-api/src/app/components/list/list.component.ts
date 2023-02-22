426399c10011183bce5bbef5d27b442aec586096
Dimpeny <guilhermezimermann.grz@gmail.com>
2023-02-22 20:51:32 -0300

    Terminado exemplo projeto-consumo-api

--- Projetos Angular/projeto-consumo-api/src/app/components/list/list.component.ts
+++ Projetos Angular/projeto-consumo-api/src/app/components/list/list.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Subject, Observable } from 'rxjs';
+import { ApiReturn, Results } from './../../models/api-return';
+import { ApiService } from './../../service/api.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
 
+  public pkmnList = new Observable<ApiReturn>();
+  public resultsList$ = new Subject<Results[] | undefined>();
+
+  constructor (public service: ApiService){}
+
+  ngOnInit(): void {
+     this.pkmnList = this.service.getPokemonList();
+     this.pkmnList.subscribe(
+      (resp) => {
+        console.log(resp);
+        this.resultsList$.next(resp.results);
+      }
+     )
+  }
 }
--- Projetos Angular/projeto-consumo-api/src/app/service/api.service.ts
+++ Projetos Angular/projeto-consumo-api/src/app/service/api.service.ts
@@ -1,3 +1,4 @@
+import { ApiReturn } from './../models/api-return';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
@@ -6,6 +7,13 @@ import { Injectable } from '@angular/core';
 })
 export class ApiService {
 
-  URL_API:string = "https://pokeapi.co/api/v2"
+  URL_API:string = "https://pokeapi.co/api/v2/pokemon"
+
   constructor(public http:HttpClient) {}
+
+  getPokemonList () {
+    return this.http.get<ApiReturn>(this.URL_API);
+  }
+
+
 }
